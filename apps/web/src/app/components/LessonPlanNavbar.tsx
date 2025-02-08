8d4ffe75e78f2dbb200d756a324153ed1049cc40
kakotyanimesh <phb21013@tezu.ac.in>
2025-02-08 19:27:56 +0530

    pushed but im cofused now

--- apps/web/src/app/components/LessonPlanNavbar.tsx
+++ apps/web/src/app/components/LessonPlanNavbar.tsx
@@ -1,18 +1,26 @@
 "use client"
 import { Button } from '@repo/ui/button';
+import LessonPlan from '@repo/ui/lessonPlan';
 import { BookCopy } from 'lucide-react';
+import { useState } from 'react';
 
 
 export default function NavbarLp(){
+    const [openModal, setOpenModal] = useState(false)
     return (
-        <div className='flex flex-col justify-between '>
-            <div className='flex flex-col gap-10'>
+        <div className='flex flex-row justify-between  md:mx-20 mx-10 my-10 items-center '>
+            <div className='flex flex-row md:gap-10 items-center'>
                 <BookCopy size={36} color="#1657c0" absoluteStrokeWidth />
-                <h1>Lesson Planner</h1>
+                <h1 className='md:text-3xl font-bold'>Lesson Planner</h1>
             </div>
             <div>
-                <Button title='createLessonPlan' varientstype='primary' onClick={() => alert("asdasd")} />
+                <Button title='create Lesson Plan' varientstype='primary' onClick={() => setOpenModal(!openModal)} />
             </div>
+            {
+                openModal && <div className='absolute flex justify-center items-center'>
+                    <LessonPlan/>
+                </div>
+            }
         </div>
     )
 }
